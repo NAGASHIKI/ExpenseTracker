b71bc80edbbc86bac077879724e4da471c81d5a4
Aymar <akslain4k@.com>
2024-04-23 23:02:56 +0100

    Initial commit

---
+++ screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import tw from "twrnc";
+
+export default function LoginScreen() {
+  return (
+    <View style={tw`flex-1 bg-blue-500`}>
+
+    </View>
+  )
+}
